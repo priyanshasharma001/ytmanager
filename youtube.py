f5ee5f018ad5972b5b0da6468f570d27c65fbb4a
priyanshasharma001 <147186433+priyanshasharma001@users.noreply.github.com>
2024-06-30 23:20:30 +0530

    Add files via upload

---
+++ youtube.py
@@ -0,0 +1,18 @@
+import sqlite3
+conn =sqlite3.connect('youtube_videos.db')
+cursor=conn.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS videos(
+               id INTEGER PRIMARY KEY,
+               name TEXT NOT NULL,
+               time TEXT NOT NULL
+
+         )
+    ''')
+
+def main():
+    while True:
+        print("")
+if __name__=="__main__":
+    main()
